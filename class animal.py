4f5b92f86268fd9104ef46ba72a97e0cc40b0654
harshitha9988 <harshitha.uppala@outlook.com>
2025-06-29 21:36:23 -0400

    class animal program

---
+++ class animal.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+class animal(ABC):
+
+    def move(self):
+            pass
+    
+class human(animal):
+    def move(self):
+         print("I can walk and run")
+
+class snake(animal):
+    def move(self):
+        print("I can crawl")
+
+class dog(animal):
+    def move(self):
+        print("I can bark")
+
+class lion(animal):
+    def move(self):
+        print("I can roar")
+
+R=human()
+R.move()
+
+k=snake()
+k.move()
+
+a=dog()
+a.move()
+
+b=lion()
+b.move()
